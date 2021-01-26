ef48bc10efdb2a993b4b8bba29fedbc7e9e0b99d
lea.arturi <lea.arturi@gmail.com>
2021-01-26 14:29:02 -0300

    Finalizado

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, NavLink, useHistory } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const Navbar = () => {
+
+    const { user, dispatch } = useContext(AuthContext);
+
+    const history = useHistory();
+
+    const handleLogout = () => {
+        dispatch({
+            type: types.logout
+        });
+
+        history.replace('/login');
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -9,7 +24,7 @@ export const Navbar = () => {
                 className="navbar-brand" 
                 to="/"
             >
-                Asociaciones
+                HeroesApp
             </Link>
 
             <div className="navbar-collapse">
@@ -46,14 +61,17 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
+
+                    <span className="nav-item nav-link text-info">
+                        { user.nombre }
+                    </span>
+
+                    <button 
+                        className="nav-item nav-link btn" 
+                        onClick={handleLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
