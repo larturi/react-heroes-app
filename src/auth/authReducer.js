5224b0de5ddbdc1afe79d37283a5964396fed5bc
lea.arturi <lea.arturi@gmail.com>
2021-01-27 16:15:49 -0300

    Avances pruebas unitarias

--- src/auth/authReducer.js
+++ src/auth/authReducer.js
@@ -8,8 +8,7 @@ import { types } from "../types/types";
 export const authReducer = (state = {}, action) => {
 
     switch (action.type) {
-        case types.login:
-            
+        case types.login:            
             return {
                 ...action.payload,
                 logged: true
